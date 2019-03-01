ca649cb4a07734e30c54a600888556173e202b75
Josh <joshkardos@gmail.com>
2019-03-01 10:14:00 -0800

    added better responsiveness

--- BitcoinFancyCars/BitcoinFancyCars/Controllers/AddViewController.swift
+++ BitcoinFancyCars/BitcoinFancyCars/Controllers/AddViewController.swift
@@ -11,46 +11,61 @@ import Alamofire
 import SwiftyJSON
 
 class AddViewController: UIViewController {
-
+    
     @IBOutlet weak var modelLabelField: UITextField!
     @IBOutlet weak var priceLabelField: UITextField!
+    
+    @IBOutlet weak var goBackButton: UIButton!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    override func viewDidLoad() {
+        goBackButton.layer.cornerRadius = 7
+        submitButton.layer.cornerRadius = 7
+    }
+    
     
     @IBAction func submit(_ sender: Any) {
         
         if modelLabelField.text!.trimmingCharacters(in: .whitespaces) != "" && priceLabelField.text!.trimmingCharacters(in: .whitespaces) != ""{
             
-                priceLabelField.isEnabled = false
-                modelLabelField.isEnabled = false
-                
+            priceLabelField.isEnabled = false
+            modelLabelField.isEnabled = false
+            
             let newCar = Car(model: modelLabelField.text!, price: Double(priceLabelField.text!)!)
-                
-                MainViewController.lamboModelsDB.append(newCar)
-                self.priceLabelField.isEnabled = true
-                self.modelLabelField.isEnabled = true
-                self.performSegue(withIdentifier: "AfterAddingCarGoHome", sender: self)
+            
+            MainViewController.lamboModelsDB.append(newCar)
+            self.priceLabelField.isEnabled = true
+            self.modelLabelField.isEnabled = true
+            
+            //TODO: - Supercar Noise
+            
+            self.performSegue(withIdentifier: "AfterAddingCarGoHome", sender: self)
             
         } else {
             
             //one of the fields is empty
             print("One of the fields was empty")
             
+            
+            //TODO: - Alert
+            //must fill both spots
         }
-	
-	
-		
-	}
-
-	func textFieldDidBeginEditing(_ textField: UITextField) {
-		
-		UIView.animate(withDuration: 0.5){
-			
-			//self.heightConstraint.constant = 308
-			//self.view.layoutIfNeeded()//if constatint or anything has changed, redraw the hole thing
-			
-		}
-	}
-	
-	
-
+        
+        
+        
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        UIView.animate(withDuration: 0.5){
+            
+            //self.heightConstraint.constant = 308
+            //self.view.layoutIfNeeded()//if constatint or anything has changed, redraw the hole thing
+            
+        }
+    }
+    
+    
+    
 }
 
--- BitcoinFancyCars/BitcoinFancyCars/Controllers/MainViewController.swift
+++ BitcoinFancyCars/BitcoinFancyCars/Controllers/MainViewController.swift
@@ -12,11 +12,16 @@ import SwiftyJSON
 import Firebase
 
 class MainViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-	
-	static let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD"//url format for api
-    var bitcoinPrice = 0.0 {
+    
+    static let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD"
+    //url format for api
+    var bitcoinPrice = Double(){
         didSet{
+            
             bitcoinPriceLabel.text = String("$\(bitcoinPrice)")
+            let rowSelected = currencyPicker.selectedRow(inComponent: 0)
+            updateRatio(carPrice: MainViewController.lamboModelsDB[rowSelected].price, row: rowSelected)
+        
         }
     }
     var timePriceLastSet = NSDate(){
@@ -24,124 +29,127 @@ class MainViewController: UIViewController, UIPickerViewDataSource, UIPickerView
             //date formatter
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "MM/dd/yyyy, hh:mm:ss"
-            var tempTime = dateFormatter.string(from: timePriceLastSet as Date)
-            timeLabel.text = "Last Refreshed at \(tempTime)"
+            let time = dateFormatter.string(from: timePriceLastSet as Date)
+            timeLabel.text = "Last Refreshed at \(time)"
         }
     }
     
-	static var lamboModelsDB = [Car(model: "Gallardo", price: 400000), Car(model:"Van", price:900)]
-	//static var lamboModelsDB = returnCars()
-	
-	var carIndexToDelete = 0
-	
-	//Pre-setup IBOutlets
-	
-	@IBOutlet weak var ratioLabel: UILabel!
-	@IBOutlet weak var bitcoinPriceLabel: UILabel!
-	@IBOutlet weak var currencyPicker: UIPickerView!
+    static var lamboModelsDB = [Car(model: "Gallardo", price: 400000), Car(model:"Van", price:900)]
+    //static var lamboModelsDB = returnCars()
+    
+    var carIndexToDelete = 0
+    
+    //Pre-setup IBOutlets
+    
+    @IBOutlet weak var ratioLabel: UILabel!
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
     @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var timeLabel:UILabel!
     
     
-	override func viewDidLoad() {
+    override func viewDidLoad() {
         
+        super.viewDidLoad()
         
-        
-		super.viewDidLoad()
-		
-		getBitcoinData(url: MainViewController.baseURL)
-		currencyPicker.delegate = self
-		currencyPicker.dataSource = self
-		refreshButton.layer.cornerRadius = 7
-        
-		
-	}
-	//TODO: Place your 3 UIPickerView delegate methods here
-	func numberOfComponents(in pickerView: UIPickerView) -> Int {//amount of columns
-		
-		return 1
-	}
-	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {//amount of rows
-		return MainViewController.lamboModelsDB.count
-	}
-	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) ->
-		//title of car
-		String? {//titles
-			return MainViewController.lamboModelsDB[row].model
-	}
-	
-	
-	func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
-		//TO DO: Choose lamborghini model
-		if MainViewController.lamboModelsDB.count > row{
-			carIndexToDelete = row
-			updateRatio(price: Double(MainViewController.lamboModelsDB[row].price).rounded(), row: row)
-		}
-	}
-	
-	func updateRatio(price: Double, row: Int){
-		
-		let ratio = bitcoinPrice/price
-		
-		ratioLabel.text = "1 Bitcoin = \(String(ratio)) \(MainViewController.lamboModelsDB[row].model)(s)"
-	}
-	
-	
-	@IBAction func deleteIt(_ sender: Any) {
-		//array must always have at least one car
-		if MainViewController.lamboModelsDB.count > 1{
-			MainViewController.lamboModelsDB.remove(at: carIndexToDelete)
-			currencyPicker.reloadAllComponents()
-		} else {
-			print("Could not delete")
-		}
-	}
-	
-
-	//
-	//    //MARK: - Networking
-	//    /***************************************************************/
-	
-	func getBitcoinData(url: String) {
-		Alamofire.request(url, method: .get)
-			.responseJSON { response in
-				if response.result.isSuccess {
-					let bitcoinJSON : JSON = JSON(response.result.value!)
-					self.updateBitcoinData(json: bitcoinJSON)
-					
-				} else {
-					self.bitcoinPriceLabel.text = "Connection Issues"
-				}
-		}
-		
-	}
-	
-	
-    @IBAction func refreshButtonPressed(_ sender: UIButton) {
+        currencyPicker.selectRow(0, inComponent: 0, animated: false)
         
         getBitcoinData(url: MainViewController.baseURL)
         
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+        refreshButton.layer.cornerRadius = 7
+        
+        
+    }
+    //TODO: Place your 3 UIPickerView delegate methods here
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {//amount of columns
+        
+        return 1
+        
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {//amount of rows
+        
+        return MainViewController.lamboModelsDB.count
+        
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) ->
+        //title of car
+        String? {//titles
+            return MainViewController.lamboModelsDB[row].model
     }
     
-	
-	
-	//    //MARK: - JSON Parsing
-	//    /***************************************************************/
-	
-	func updateBitcoinData(json : JSON) {
-		if let bitcoinResult = json["ask"].double {
-			self.bitcoinPrice = bitcoinResult
-            timePriceLastSet = NSDate()
-		}
-		else{
-			bitcoinPriceLabel.text = "Price unavailable"
-		}
-	}
-	
-	
-	
-	
-	
-}
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        //TO DO: Choose lamborghini model
+        if MainViewController.lamboModelsDB.count > row{
+            carIndexToDelete = row
+            updateRatio(carPrice: MainViewController.lamboModelsDB[row].price, row: row)
+        }
+    }
+    
+    @IBAction func deleteIt(_ sender: Any) {
+        //array must always have at least one car
+        if MainViewController.lamboModelsDB.count > 1{
+            MainViewController.lamboModelsDB.remove(at: carIndexToDelete)
+            currencyPicker.reloadAllComponents()
+            updateRatio(carPrice: bitcoinPrice ,row: currencyPicker.selectedRow(inComponent: 0))
+            
+        } else {
+            
+            //TODO: - Alerrt
+            //alert here that says cant delete, must have at least one car in picker
+            
+        }
+    }
+    
+    @IBAction func refreshButtonPressed(_ sender: UIButton) {
+        
+        getBitcoinData(url: MainViewController.baseURL)
+        //TODO: - resfresh noise
+        
+    }
+    
+    
+    func updateRatio(carPrice: Double, row: Int){
 
+        var ratio = bitcoinPrice/carPrice
+        ratio = round(100000 * ratio) / 100000
+        print("Ratio \(ratio)")
+        ratioLabel.text = "1 Bitcoin = \(String(ratio)) \(MainViewController.lamboModelsDB[row].model)(s)"
+        
+    }
+    
+    //
+    //    //MARK: - Networking
+    //    /***************************************************************/
+    func getBitcoinData(url: String) {
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+                    let bitcoinJSON : JSON = JSON(response.result.value!)
+                    self.updateBitcoinData(json: bitcoinJSON)
+                    
+                } else {
+                    self.bitcoinPriceLabel.text = "Connection Issues"
+                }
+        }
+        
+    }
+    
+    //    //MARK: - JSON Parsing
+    //    /***************************************************************/
+    func updateBitcoinData(json : JSON) {
+        if let bitcoinResult = json["ask"].double {
+            
+            self.bitcoinPrice = bitcoinResult
+            
+            timePriceLastSet = NSDate()
+    
+        }
+        else{
+            bitcoinPriceLabel.text = "Price unavailable"
+        }
+    }
+}
 
